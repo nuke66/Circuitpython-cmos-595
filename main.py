8bc72562c64f2aca62b2ee5a01878bbf5d122f4c
slade harker <slade_harker@hotmail.com>
2020-03-11 08:08:55 +1000

    save commit

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-"""
-This example program connects a MCP3008 analog to digital converter using CircuitPython 
-on an Adafruit ItsyBitsy M4 Express. 
+""" 
+CircuitPython example hooking up CMOS 74HC595 SIPO to give you 8 digital outputs.  You can daisy
+chain multiple units to give you move outputs.
 
-MCP3008 Pin   SPI   ItsybitsyM0
-------------+-----+----------------
-13 (Clk)      SCK    SCK (SCK)
-12 (Dout)     MISO   MI  (MISO)
-11 (Din)	  MOSI   MO  (MOSI)
-10 (Cs)		  CS     D5  (Any free digital pin)
+In this case I used an Adafruit ItsyBitsy M4 but your more likely to use it on a 
+board with limited pins like the Trinket M0
+
+cmos 595         pin   Itsybitsy M4
+----------------+-----+------------
+serial in         ?      D?
+clock             ?      D?
+latch             ?      D?
 
 """
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+
 import time
+import board
+import digitalio
+import simpleio
 
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D5) # chip select
-mcp = MCP.MCP3008(spi, cs) 
+# set up clock, data, and latch pins
+clk = digitalio.DigitalInOut(board.D3)
+clk.direction = digitalio.Direction.OUTPUT
+data = digitalio.DigitalInOut(board.D2)
+data.direction = digitalio.Direction.INPUT
+latch = digitalio.DigitalInOut(board.D0)
+latch.direction = digitalio.Direction.OUTPUT
 
-p0 = AnalogIn(mcp, MCP.P0) # channel0
+# insert a small pause after keypress
+def qpause():
+    time.sleep(0.2)
 
 while True:
-    print('Value:   {:05}'.format(p0.value))
-    print('Voltage: {:1.2}V'.format(p0.voltage))
-    time.sleep(0.1)
+    latch.value = False
+    #rs = not(rs)
+    #rs= simpleio.shift_in(data, clk) 
+    #print("output: {0:#010b} {0}".format(rs),end="")
+    latch.value = True
+
+    time.sleep(0.05)
